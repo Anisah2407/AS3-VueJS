4b0eb84f2dee50e7ed6ce308df8085cbcedae9aa
Noranisah <22ftt1492@student.pb.edu.bn>
2024-04-18 09:44:20 +0800

    new text

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,17 @@
       <p class="text-gray-800 mb-4">22FTT1492 | Web Developer | DWDT08</p>
       <!-- Add hobbies, interests, etc. here -->
       <p class="text-gray-700 mb-4">
-        I'm a passionate soul who thrives on adventure and creativity. Rugby is
-        my adrenaline-fueled escape, where the rush of the game and the
-        camaraderie of the team ignite my spirit. When I'm not on the field,
-        you'll often find me exploring the globe, soaking in the sights, sounds,
-        and flavors of new cultures. Traveling fuels my curiosity and feeds my
-        soul with unforgettable experiences. And when I'm back home, there's
-        nothing I love more than experimenting in the kitchen, concocting
-        delicious dishes inspired by my culinary adventures. Whether it's
-        whipping up a hearty stew or mastering the art of a delicate dessert,
-        cooking is my way of expressing love and sharing joy with others.
+        hello world! I'm a passionate soul who thrives on adventure and
+        creativity. Rugby is my adrenaline-fueled escape, where the rush of the
+        game and the camaraderie of the team ignite my spirit. When I'm not on
+        the field, you'll often find me exploring the globe, soaking in the
+        sights, sounds, and flavors of new cultures. Traveling fuels my
+        curiosity and feeds my soul with unforgettable experiences. And when I'm
+        back home, there's nothing I love more than experimenting in the
+        kitchen, concocting delicious dishes inspired by my culinary adventures.
+        Whether it's whipping up a hearty stew or mastering the art of a
+        delicate dessert, cooking is my way of expressing love and sharing joy
+        with others.
       </p>
     </div>
   </div>
